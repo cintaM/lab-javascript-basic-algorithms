4adfdfcac7f00ed01fe60307faef4bb41f076560
Cinta Marquez <mariadelacinta.marquez@gmail.com>
2022-07-23 14:45:48 +0200

    segunda

--- js/index.js
+++ js/index.js
@@ -6,6 +6,15 @@ console.log(`The driver's name is ${hacker1}`)
 const hacker2 = "Emily"
 console.log(`The driver's name is ${hacker2}`)
 // Iteration 2: Conditionals
+const hacker1 = "Tom"
+const hacker2 = "David"
+if (hacker1.length > hacker2.length){
+  console.log (`The driver has the longest name, it has ${hacker1.length} characters.`)
+} else if (hacker2.length > hacker1.length){
+   console.log (`The driver has the longest name, it has ${hacker2.length} characters.`)
+} else if (hacker1.length === hacker2.length){
+  console.log (`Wow, you both have equally long names, ${hacker2.length} characters!`)
+}
 
 
 // Iteration 3: Loops
