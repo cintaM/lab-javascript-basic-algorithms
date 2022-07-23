71d6007b975e7d46fbca8399e8832d1896aad912
Cinta Marquez <mariadelacinta.marquez@gmail.com>
2022-07-23 15:52:11 +0200

    doing

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+console.log("I'm ready!");
 // Iteration 1: Names and Input
 //
 
@@ -6,8 +7,7 @@ console.log(`The driver's name is ${hacker1}`)
 const hacker2 = "Emily"
 console.log(`The driver's name is ${hacker2}`)
 // Iteration 2: Conditionals
-const hacker1 = "Tom"
-const hacker2 = "David"
+
 if (hacker1.length > hacker2.length){
   console.log (`The driver has the longest name, it has ${hacker1.length} characters.`)
 } else if (hacker2.length > hacker1.length){
@@ -16,7 +16,22 @@ if (hacker1.length > hacker2.length){
   console.log (`Wow, you both have equally long names, ${hacker2.length} characters!`)
 }
 
-
 // Iteration 3: Loops
+// 3.1 
+let hacker3 ="Jonh"
+let words = hacker3.toUpperCase();
+console.log(words.split(''));
 
-console.log("I'm ready!");
+// 3.2
+const palabraDesordenada = cadena => {
+    const PalabraArreglo = cadena.split("");
+    PalabraArreglo.sort();
+    const cadenaOrdenada = PalabraArreglo.join("");
+    return cadenaOrdenada;
+};
+const hacker3 = "john";
+const conLetrasOrdenadas = palabraDesordenada(hacker3);
+console.log(conLetrasOrdenadas);
+
+// 3.3
+
