55c3082d22b1b27b73c8e0a3e9b35371553bc313
Cinta Marquez <mariadelacinta.marquez@gmail.com>
2022-07-23 14:30:47 +0200

    primera

--- js/index.js
+++ js/index.js
@@ -1,5 +1,10 @@
 // Iteration 1: Names and Input
 //
+
+const hacker1 = "Tom"
+console.log(`The driver's name is ${hacker1}`)
+const hacker2 = "Emily"
+console.log(`The driver's name is ${hacker2}`)
 // Iteration 2: Conditionals
 
 
